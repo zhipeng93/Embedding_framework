4bf92bd546790f27bf4075964139633c94869272
zhipengubuntu <zhangzhipeng@pku.edu.cn>
2017-03-30 07:53:17 -0400

    fixed BUG for katz--misuse the transpose matrix, which causes the recall to be very high

--- src/main/java/SimMeasures/Katz.java
+++ src/main/java/SimMeasures/Katz.java
@@ -12,11 +12,13 @@ public class Katz extends SimBase{
      * This is a paper.
      */
     double beta = 0.1;
+    LinkedList<Integer> reverseGraph[];
     LinkedList<Integer> graph[];
     int ITER_NUM = 3;
     int node_num;
     public Katz(LinkedList<Integer> graph[], int node_num){
-        this.graph = genReverseGraph(graph, node_num);
+        this.graph = graph;
+        this.reverseGraph = genReverseGraph(graph, node_num);
         this.node_num = node_num;
     }
     @Override
@@ -25,25 +27,32 @@ public class Katz extends SimBase{
     @Override
     public double[] singleSourceSim(int qv){
         double rs[][] = new double[2][node_num];
-        LinkedList<Integer> qv_adj = graph[qv];
         for(int kk = 0; kk < ITER_NUM; kk++){
             Arrays.fill(rs[kk & 1], 0);
             for(int i = 0; i< node_num; i++){
-                Iterator iter = graph[i].iterator();
+                Iterator iter = reverseGraph[i].iterator();
 
                 while(iter.hasNext()) {
                     int neigh = (Integer) iter.next();
                     rs[kk & 1][i] += rs[1 - (kk & 1)][neigh];
                 }
-                /**
-                 * NEEDs to reorganize.
-                 */
-                if(qv_adj.contains(i))
-                    rs[kk & 1][i] += 1;
                 rs[kk & 1][i] *= beta;
             }
+            Iterator iterator = graph[qv].iterator();
+            /**
+             * if you use the result of reverse graph, the link predication result is very high.
+             * Similar to that of Rooted PageRank, maybe 0.9 recall.
+             * But it is a bug. May be we can add a issue that if there is an edge from u to v, then there
+             * is possibly an edge from v to u.
+             */
+            while(iterator.hasNext()){
+                rs[kk & 1][(Integer)iterator.next()] += 1 * beta;
+            }
+//                rs[kk & 1][qv] += 1;
 
         }
+//        for(int i=0;i< node_num; i++)
+//            rs[ITER_NUM & 1][i] -= 1;
         return rs[ITER_NUM &1];
     }
 
