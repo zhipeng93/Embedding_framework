dd0337bf4f5732fef552d7308fbdf6a6a5454bf4
zhipengubuntu <zhangzhipeng@pku.edu.cn>
2017-04-17 22:11:32 -0400

    weighted RPR, inspired by S2R and R2S, which jointly learns importance ranking and relevance ranking

--- src/main/java/SimMeasures/PersonalizedPageRank.java
+++ src/main/java/SimMeasures/PersonalizedPageRank.java
@@ -1,9 +1,6 @@
 package SimMeasures;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * THIS is ROOTED PageRank, not a Personalized PageRank. Maybe a *special case*.
@@ -21,6 +18,8 @@ public class PersonalizedPageRank extends SimBase{
         init_out_degree_table();
         /* changing the formula of Rooted PageRank.*/
         init_in_degree_table();
+        init_random_walk_table();
+        init_pagerank();
     }
     public PersonalizedPageRank(LinkedList<Integer> graph[], int node_num,
                double restart_rate, int max_step){
@@ -35,6 +34,51 @@ public class PersonalizedPageRank extends SimBase{
     int node_num;
     double restart_rate = 0.9;
     int max_step = 3;
+    double pagerank[][];
+    int pagerank_iter = 10;
+    /*store pagerank value for each node */
+    void init_pagerank(){
+        double damping_factor = 0.15;
+        pagerank = new double[2][node_num];
+        Random random = new Random(System.currentTimeMillis());
+        for(int i=0; i < node_num; i ++)
+            pagerank[0][i] = random.nextDouble();
+        int iter = 0;
+        while(iter ++ < pagerank_iter){
+            //use pagerank[1 - (iter & 1)] to compute pagerank[(iter & 1)]
+            for(int i = 0; i < node_num; i++){
+                //update pagerank[iter & 1][i]
+                pagerank[iter & 1][i] = damping_factor / node_num;
+                Iterator graph_iterator = reversedGraph[i].iterator();
+                while(graph_iterator.hasNext()){
+                    int in_neigh_i = (Integer) graph_iterator.next();
+                    int in_neigh_i_out_deg = graph[in_neigh_i].size();
+                    pagerank[iter & 1][i] += (1 - damping_factor) * pagerank[1 - (iter & 1)][in_neigh_i] / in_neigh_i_out_deg;
+                }
+            }
+
+        }
+        // the result is store in pagerank[1 - (iter & 1)][*], but it does not matter.
+    }
+    /* Store number of random walks starting from each node*/
+    int random_walk_num[][];
+    void init_random_walk_table(){
+        random_walk_num = new int[node_num][max_step];
+        /**
+         * random_walk[nodeId][step]: number of random walks starting from each node with step=step
+         */
+        for(int i=0; i< node_num; i++)
+            random_walk_num[i][0] = 1;
+        for(int step=1; step < max_step; step++){
+            for(int node = 0; node < node_num; node ++){
+                Iterator iteror= graph[node].iterator();
+                while(iteror.hasNext()){
+                    random_walk_num[node][step] += random_walk_num[(Integer)iteror.next()][step - 1];
+                }
+                random_walk_num[node][step] += random_walk_num[node][step - 1]; //cumulate each step.
+            }
+        }
+    }
     @Override
     public double[] singleSourceSim(int qv){
         double p[][] = new double[2][node_num];
@@ -54,7 +98,10 @@ public class PersonalizedPageRank extends SimBase{
             p[1 - (step & 1)][qv] += restart_rate;
         }
 //        for(int i=0; i< node_num; i++)
-//            p[max_step & 1][i] *= out_degree[i];
+//            p[max_step & 1][i] *= Math.sqrt(random_walk_num[qv][max_step - 1]);
+//            p[max_step & 1][i] *= Math.pow(out_degree[qv], 1.05);
+//            p[max_step & 1][i] *= random_walk_num[qv][max_step - 1];
+//            p[max_step & 1][i] *= pagerank[1 - (1 & pagerank_iter)][qv];
         return p[max_step & 1];
     }
 
